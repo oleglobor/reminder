3ee763a4dcb436a00ca8349f6bd49d869c460868
oleglobor <olegromanovichlviv@gmail.com>
2021-09-07 21:39:02 +0300

    LITS

--- src/Reminder.java
+++ src/Reminder.java
@@ -1,2 +1,25 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class Reminder {
+    Timer timer;
+
+    public Reminder(int seconds) {
+        timer = new Timer();
+        timer.schedule(new RemindTask(), seconds * 1000);
+    }
+
+    class RemindTask extends TimerTask {
+        public void run() {
+            System.out.println("Час вичерпано!");
+            timer.cancel();
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Підготовка!");
+        new Reminder(5);
+        System.out.println("Запусскаємо таймер");
+    }
 }
